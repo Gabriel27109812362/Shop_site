5e452aa563c0a7ff82c19bd3c19d57517a610683
DELL-PRECISION-\Gabriel Opolski <gabriel.opolski98362@gmail.com>
2019-02-18 13:32:16 +0100

    Add main components
    Add About, Catalog, Header, Home components, BrowserRouter and global
    style components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import styled, {createGlobalStyle} from 'styled-components';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/Header/Header';
+
+const GlobalStyle = createGlobalStyle`
+
+  body{
+      font-family: "Catamaran", "Helvetica Neue", Helvetica, Arial, sans-serif;
+      font-size: 14px;
+      color: #000;
+      margin: 0;
+      background-color: #eee;
+      text-align: left;
+}
+`;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+   render() {
+      return (
+         <>
+            <GlobalStyle/>
+            <Router>
+               <div>
+                  <Route path='/home' component = />
+                  <Route path='/catalog'/>
+                  <Route path='/about'/>
+               </div>
+            </Router>
 
-      </div>
-    );
-  }
+
+         </>
+      );
+   }
 }
 
 export default App;
