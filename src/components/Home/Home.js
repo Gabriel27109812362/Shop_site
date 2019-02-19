09b1a28461bc8d429a537a589fb56d8d47287fcb
DELL-PRECISION-\Gabriel Opolski <gabriel.opolski98362@gmail.com>
2019-02-19 12:10:05 +0100

    Add elements & style elements
    HeaderBig and HeaderSmall component to home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const HeaderBig = styled.h1`
+    font-size: 30px;
+    text-transform: uppercase;
+    padding: 15px 0;
+`;
+
+const HeaderSmall = styled.h2`
+    font-size: 22px;
+    text-transform: uppercase;
+    padding: 5px 0;
+`;
+
 const Home = () => {
-   return <p> Home</p>;
+   return (
+      <div>
+         <HeaderBig>Welcome to our store </HeaderBig>
+         <HeaderSmall>Desktops</HeaderSmall>
+      </div>
+      
+
+   );
 };
 
 export default Home;
