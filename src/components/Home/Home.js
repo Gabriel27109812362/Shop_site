a38cbaefca07402063fcb64d4f7f4f84d975bc34
DELL-PRECISION-\Gabriel Opolski <gabriel.opolski98362@gmail.com>
2019-02-19 13:56:45 +0100

    Finish home component
    Add props to product component, add procusts to product list in home
    component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Products from '../../components/Products/Products'
+import Product from '../../components/Product/Product';
 
 const HeaderBig = styled.h1`
     font-size: 30px;
@@ -14,15 +14,30 @@ const HeaderSmall = styled.h2`
     padding: 5px 0;
 `;
 
+const Products = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
 const Home = () => {
    return (
       <div>
          <HeaderBig>Welcome to our store </HeaderBig>
          <HeaderSmall>Desktops</HeaderSmall>
-         <Products>Products</Products>
+         <Products>
+               <Product image='https://i.imgur.com/s7zey8T.jpg' name='iMac 27 Retina' alt='Apple iMac 27 Retina' price='$2099.99'/>
+               <Product image='https://i.imgur.com/CCanSEL.jpg' name='Surface Studio' alt='Microsoft Surface Studio' price='$3749.99'/>
+               <Product image='https://i.imgur.com/qQx2zmv.jpg' alt='Dell Inspion 23' name='Dell Inspion 23' price='$1987.99'/>
+               <Product image='https://i.imgur.com/7Zp9oiW.jpg' name='Lenovo IdeaCenter' alt='Lenovo IdeaCenter' price='$2487.99'/>
+         </Products>
          <HeaderSmall>Tablets</HeaderSmall>
+         <Products>
+               <Product image='https://i.imgur.com/uROXKvy.jpg' name='Mi Pad 2' alt='Xiamomi Mi Pad 2' price='$899.99'/>
+               <Product image='https://i.imgur.com/9bliZbf.jpg' name='Apple iPad Air' alt='Apple iPad Air' price='$1249.99'/>
+               <Product image='https://i.imgur.com/f6vLn7N.jpg' name='Asus Transformer' alt='Asus Transformer' price='$1157.99'/>
+               <Product image='https://i.imgur.com/NUsqPRC.jpg' name='iPad Mini' alt='iPad Mini' price='$480.99'/>
+         </Products>
       </div>
-
 
    );
 };
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Frame = styled.div`
-  
+const PrFrame = styled.div`
+    width: 25%;
+    background: white;
+    margin: 1%;
+    text-align: center;
+    flex: 1 0 23%;
 `;
-
+const Picture = styled.img`
+    display: block;
+    width: 90%;
+    height: auto;
+    margin: 0 auto;
+`;
+const Price = styled.p`
+    display: inline-block;
+    font-size: 24px;
+    line-height: 28px;
+    color: #00a1b4;
+    padding: 3px 0px;
+`;
 const Product = props => {
 
    return (
+      <PrFrame>
+         <Picture src={props.image} alt={props.alt}/>
+         <Price>{props.price}</Price>
+         <h3>{props.name}</h3>
+      </PrFrame>
 
    );
-
 };
+
+export default Product;
