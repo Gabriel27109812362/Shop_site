e51aa076993b943dd51b7574f7ec825941159098
DELL-PRECISION-\Gabriel Opolski <gabriel.opolski98362@gmail.com>
2019-02-19 14:16:54 +0100

    Add columns and styles to columns in catalog

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const HeaderBig = styled.h1`
+    font-size: 30px;
+    text-transform: uppercase;
+    padding: 15px 0;
+`;
+
+const MainDiv = styled.div`
+    display: flex;
+`;
+const ColumnLeft = styled.div`
+    width: 25%;
+`;
+const ColumnRight = styled.div`
+    width: 75%;
+`;
+
 const Catalog = () => {
-   return <p>Catalog</p>;
+   return (
+      <MainDiv>
+         <HeaderBig>Catalog</HeaderBig>
+         <ColumnLeft>
+
+         </ColumnLeft>
+         <ColumnRight>
+
+         </ColumnRight>
+
+      </MainDiv>
+
+   );
 };
 
 export default Catalog;
